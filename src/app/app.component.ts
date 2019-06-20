2cd3a58d1dd2162e6068be48c52fd9880b56fbec
samuksilv <samuel.silv.carvalho@gmail.com>
2019-06-20 19:08:29 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,13 @@ export class AppComponent  {
    /**
    * apesar da promise não ser consumida por um then() , a promisse executa todo o seu bloco de código. 
    * A promise mantém um estado compartilhado, a execução da promise acontece uma unica vez e quando chamo o .then() o valor não é executado novamente
-   */
-  
-    // new Promise((resolve)=>{
-    //   console.warn('iniciando a Promise');      
-    //   resolve(1);
-    //   resolve(2);
-    // })
-    // .then(x=> console.log('Promisse', x));
+   */  
+    new Promise((resolve)=>{
+      console.warn('iniciando a Promise');      
+      resolve(1);
+      resolve(2);
+    })
+    .then(x=> console.log('Promisse', x));
 
     /**
      * para que o bloco de código do observable seja executado é necessário chamar .subscribe()
@@ -30,7 +29,7 @@ export class AppComponent  {
       Observable.next(1);
       Observable.next(2);
     })
-    // .subscribe(x=> console.log('Observable', x));
+    .subscribe(x=> console.log('Observable', x));
 
  }
 
